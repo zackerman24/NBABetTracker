18aaeb068db5f524eda602dc4f2c34d4da4b0aa3
Zachary Ackerman <zackerman24@gmail.com>
2020-03-28 16:51:10 -0400

    Pull in the latest NBA standings.

--- scraper.py
+++ scraper.py
@@ -1,29 +1,48 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+Next Steps:
+1. Add function save the table down.
+2. Rewrite existing code into a function that scrapes.
+3. Manipulate the data table to create necessary columns (e.g. just wins for
+                                                          total and monthly)
+4. Create necessary tables to translate team wins into "Owner" wins
+5. Create graphs to show trends over time (monthly)
+
+"""
+
 import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
-comm = re.compile("<!--|--!>")
+import datetime
 
-year = 2020
 
-url = "https://www.basketball-reference.com/leagues/NBA_{}_standings.html".format(year)
+def pull_current_standings(year):
+    """Pulls NBA standings at the current point in time."""
+    
+    comm = re.compile("<!--|--!>")
+    url = "https://www.basketball-reference.com/leagues/NBA_{}_standings.html".format(year)
+    page = requests.get(url).content.decode('utf-8')
+    soup = BeautifulSoup(re.sub("<!--|-->","",page),features='lxml')
+    standings = soup.find('table', {'id':'expanded_standings'})
+    
+    header_row = standings.find_all('tr')[1]
+    headers = [th.get_text() for th in header_row.find_all('th')][1:]
+    rows = standings.find_all('tr')[2:]
+    team_stats = [[td.get_text() for td in rows[i].find_all('td')]
+                  for i in range(len(rows))]
 
-page = requests.get(url).content.decode('utf-8')
+    nba_standings = pd.DataFrame(team_stats, columns = headers)
+    print(nba_standings)
+    return nba_standings
 
-soup = BeautifulSoup(re.sub("<!--|-->","",page),features='lxml')
+def save_current_standings(current_standings):
+    """Saves down the latest pull for current and future reference."""
+    filename = 'Standings {}.pkl'.format(datetime.date.today())
+    current_standings.to_pickle(filename)
 
-standings = soup.find('table', {'id':'expanded_standings'})
+current_pull = pull_current_standings(2020)
 
-header_row = standings.find_all('tr')[1]
-
-headers = [th.get_text() for th in header_row.find_all('th')][1:]
-
-rows = standings.find_all('tr')[2:]
-
-team_stats = [[td.get_text() for td in rows[i].find_all('td')]
-              for i in range(len(rows))]
-
-nba_standings = pd.DataFrame(team_stats, columns = headers)
-print(nba_standings)
+save_current_standings(current_pull)
