dd2195991e6dfda2df937b379617fa04db2503a5
Ackerman <AckermanZ@sunriseseniorliving.com>
2020-04-02 17:27:35 -0400

    '4-2-2020'

--- group_format.py
+++ group_format.py
@@ -3,19 +3,34 @@
 
 import pandas as pd
 from scraper import *
-from reference import OwnerMatch
+from reference import *
 import glob
 import os
 
 def load_latest_table():  
     """Pulls in the pickle file that was last saved."""
     
-    list_of_files = glob.glob('/Users/Zackerman24/Desktop/NBABetTracker/*.pkl')
+    list_of_files = glob.glob('*.pkl')
     latest_pull = max(list_of_files, key=os.path.getctime)
     latest_table = pd.read_pickle(latest_pull)
     return latest_table
 
+# table = load_latest_table())
+
 #Code to begin reformatter
 
 def format_table(original_table):
-    original_table['Wins'] = current_pull['Overall'].str.extract('(\d+)-', expand=False).astype(int)
+    """Formats table to include necessary group attributes, such as owner & round."""
+    
+    original_table['Wins'] = original_table['Overall'].str.extract('(\d+)-', expand=False).astype(int)
+    original_table ['Owner'] = original_table['Team'].map(OwnerMatch)
+    #Add Round column here to allow for graph showing both team results
+    original_table.dropna(subset=['Owner'], inplace=True)
+    return original_table
+
+def sum_table(formatted_table):
+    """Creates table showing standings by total wins."""
+    
+    summed_table = formatted_table.groupby(['Owner'])['Wins'].sum()
+    summed_table.sort_values(ascending=False, inplace=True)
+    return summed_table
--- reference.py
+++ reference.py
@@ -34,3 +34,36 @@ OwnerMatch = {
     'Utah Jazz': 'Griffin', 
     'Washington Wizards': None
     }
+
+RoundMatch = {
+    'Atlanta Hawks': 'Neil', 
+    'Boston Celtics': 'Zack', 
+    'Brooklyn Nets': 'Ian', 
+    'Charlotte Hornets': None, 
+    'Chicago Bulls': None, 
+    'Cleveland Cavaliers': None, 
+    'Dallas Mavericks': 'Grant', 
+    'Denver Nuggets': 'Simar', 
+    'Detroit Pistons': None, 
+    'Golden State Warriors': None, 
+    'Houston Rockets': 'John', 
+    'Indiana Pacers': 'Griffin', 
+    'Los Angeles Clippers': 'Sebastian', 
+    'Los Angeles Lakers': 'Ian', 
+    'Memphis Grizzlies': None, 
+    'Miami Heat': 'Eric', 
+    'Milwaukee Bucks': 'Neil', 
+    'Minnesota Timberwolves': 'Simar', 
+    'New Orleans Pelicans': None, 
+    'New York Knicks': None, 
+    'Oklahoma City Thunder': 'Dawson', 
+    'Orlando Magic': 'John', 
+    'Philadelphia 76ers': 'Dawson', 
+    'Phoenix Suns': 'Zack', 
+    'Portland Trail Blazers': 'Sebastian', 
+    'Sacramento Kings': None, 
+    'San Antonio Spurs': 'Eric', 
+    'Toronto Raptors': 'Grant', 
+    'Utah Jazz': 'Griffin', 
+    'Washington Wizards': None
+    }
